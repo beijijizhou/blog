0c9cf8905b3a79cb9476135256a342fb6f9325b5
“beijijizhou” <hongzhong.hu@stonybrook.edu>
2025-05-05 13:32:15 -0400

    update: add home button

--- src/Blog/Blog.tsx
+++ src/Blog/Blog.tsx
@@ -7,6 +7,13 @@ function Blog() {
     <div className="bg-slate-900 py-24 sm:py-32 w-full">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-4xl">
+          <Link to="/">
+            <button className="mt-8 rounded-md bg-slate-700 px-4 py-2 text-white font-semibold hover:bg-slate-600 transition-colors duration-200">
+              Home
+            </button>
+          </Link>
+
+
           <h2 className="text-pretty text-4xl font-semibold tracking-tight text-white sm:text-5xl">
             From the Blog
           </h2>
