a4bb4f55d3093ba462c8a41357df12d432682e7e
“beijijizhou” <hongzhong.hu@stonybrook.edu>
2025-05-06 14:50:38 -0400

    config: add blog route to vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { Link } from 'react-router-dom'
 import './App.css'
-// import { useState } from 'react'
 
 function App() {
-  // const [count, setCount] = useState(0)
   return (
     <div>
       <Link to="/blog">
@@ -11,15 +9,6 @@ function App() {
           className="rounded-md bg-blue-600 px-4 py-2 text-white font-semibold hover:bg-blue-500 transition-colors duration-200"
         >Go to Blog</button>
       </Link>
-      {/* <div className="mt-4">
-        <button
-          onClick={() => setCount(count + 1)}
-          className="rounded-md bg-blue-600 px-4 py-2 text-white font-semibold hover:bg-blue-500 transition-colors duration-200"
-        >
-          Increment Count
-        </button>
-        <p className="mt-2 text-white">Count: {count}</p>
-      </div> */}
     </div>
   )
 }
